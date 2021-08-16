4828e96682def86436f4c864bc946946ea2f0410
muhammadhafifalbusyro <muhammadhafifalbusyro@gmail.com>
2021-08-16 10:43:32 +0700

    tele

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TelegramLoginButton from "react-telegram-login";
 
 function App() {
+  const handleTelegramResponse = (response) => {
+    console.log(response);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +13,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TelegramLoginButton
+          dataOnauth={handleTelegramResponse}
+          botName="OdauBot"
+        />
       </header>
     </div>
   );
